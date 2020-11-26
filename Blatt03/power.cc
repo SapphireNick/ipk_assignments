74fb87885b20912b646ccaf539f5d905260383a9
Dennis Jakob <git@djakob.me>
2020-11-26 20:23:41 +0100

    added my files

---
+++ Blatt03/power.cc
@@ -0,0 +1,39 @@
+namespace power
+{
+	//a
+	/**
+	* @brief: calculates q to the nth power iteratively
+	*
+	* @param: int q
+	*       : unsigned n
+	*
+	* @return: int
+	*/
+	int iterative(int q, int n)
+	{
+		int r{ 1 };
+		for (int i = 0; i < n; ++i) {
+			r *= q;
+		}
+		return r;
+	}
+
+	//b, c
+	/**
+	* @brief: calculates q to the nth power recursively
+	*
+	* @param: int q
+	*       : unsigned n
+	*
+	* @return: int
+	*/
+	double recursive(double q, int n)
+	{
+		if (n) {
+			double tmp{ recursive(q, n / 2) };
+			return (n % 2 ? q : 1) * tmp * tmp;
+		}
+		return 1;
+	}
+} //end namespace power
+
---
+++ Blatt03/power.h
@@ -0,0 +1,10 @@
+#ifndef POWER_H_DHA1ROXG
+#define POWER_H_DHA1ROXG
+
+namespace power
+{
+	int iterative(int q, int n);
+	double recursive(double q, int n);
+}
+
+#endif /* end of include guard: POWER_H_DHA1ROXG */
