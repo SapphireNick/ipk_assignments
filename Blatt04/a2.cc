9a253d174d045340a3ea3b081ed2e1397ff8b37d
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2020-11-28 23:11:27 +0100

    First draft for A2

---
+++ Blatt04/a2.cc
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <algorithm>
+
+std::vector<std::array<double, 2>> read_points_from_file(std::string filename);
+bool sort_by_y(std::array<double, 2> a, std::array<double, 2> b);
+bool sort_by_angle(std::array<double, 2> a, std::array<double, 2> b);
+double calc_det_T(std::array<double, 2> a, std::array<double, 2> b, std::array<double, 2> c);
+void convex_hull(std::vector<std::array<double, 2>>& points);
+void write_points_to_file(std::vector<std::array<double, 2>>& points);
+
+int main(int argc, char** argv)
+{
+    std::string file_path;
+    std::cout << "Path to file: " << std::flush;
+    std::cin >> file_path;
+
+    std::vector<std::array<double, 2>> points = read_points_from_file(file_path);
+
+    convex_hull(points);
+    write_points_to_file(points);
+}
+
+std::vector<std::array<double, 2>> read_points_from_file(std::string filename)
+{
+    std::vector<std::array<double, 2>> out;
+    std::array<double, 2> row;
+    std::ifstream file(filename);
+
+    while(!file.eof())
+    {
+        file >> row[0];
+        file >> row[1];
+        out.push_back(row);
+    }
+
+    return out;
+}
+
+bool sort_by_y(std::array<double, 2> a, std::array<double, 2> b)
+{
+    if (a[1] < b[1])
+        return true;
+    if ((a[1] == b[1]) && (a[0] < b[0]))
+        return true;
+    else
+        return false;
+}
+
+bool sort_by_angle(std::array<double, 2> a, std::array<double, 2> b)
+{
+    if ((a[0]*b[1] - a[1]*b[0]) > 0)
+        return true;
+    if ((a[0]*b[1] - a[1]*b[0]) == 0 && std::abs(a[0]) > std::abs(b[0]))
+        return true;
+    else
+        return false;
+}
+
+double calc_det_T(std::array<double, 2> a, std::array<double, 2> b, std::array<double, 2> c)
+{
+    return (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1]);
+}
+
+void convex_hull(std::vector<std::array<double, 2>>& points)
+{
+    std::vector<std::array<double, 2>> stack;
+    std::sort(points.begin(), points.end(), sort_by_y);
+
+    int n = points.size();
+    for(int i = 1; i < n; i++)
+    {
+        points[i][0] -= points[0][0];
+        points[i][1] -= points[0][1];
+    }
+
+    std::sort(points.begin() + 1, points.end(), sort_by_angle);
+
+    for(int i = 1; i < n; i++)
+    {
+        points[i][0] += points[0][0];
+        points[i][1] += points[0][1];
+    }
+
+    stack.push_back(points[0]);
+    stack.push_back(points[1]);
+
+    int i = 0;
+    while(i < n)
+    {
+        double tmp = calc_det_T(stack[i - 2], stack[i - 1], points[i]);
+        if (tmp < 0)
+        {
+            stack.push_back(points[i]);
+            i++;
+        }
+        else if (stack.size() == 2)
+        {
+            stack.push_back(points[i]);
+            i++;
+        }
+        else if (tmp > 0)
+        {
+            stack.pop_back();
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    points = stack;
+}
+
+void write_points_to_file(std::vector<std::array<double, 2>>& points)
+{
+    std::ofstream file("hull.txt");
+
+    for(auto& entry : points)
+    {
+        file << entry[0] << " " << entry[1] << std::endl;
+    }
+}
