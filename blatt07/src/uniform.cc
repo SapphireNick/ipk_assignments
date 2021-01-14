eddfe4e6de9de04ba86436e358cd55768bbf5623
Dennis Jakob <git@djakob.me>
2021-01-14 19:00:20 +0100

    added b7

---
+++ blatt07/src/uniform.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "io.hh"
+#include "statistics.hh"
+
+int main(int argc, char** argv)
+{
+	auto v = uniform_distribution(0, 100, 0, 1);
+
+	for (auto&& e : v) {
+		std::cout << e << ' ';
+	}
+	double m{mean(v)}, mom{moment(v, 2)}, stddev{standard_deviation(v)};
+	std::cout << '\n';
+	std::cout << "mean: " << m << '\n';
+	std::cout << "median: " << median(v) << '\n';
+	std::cout << "2nd moment: " << mom << '\n';
+	std::cout << "standard deviation: " << stddev << '\n';
+	std::cout << (stddev * stddev == mom - m * m) << '\n';
+	
+	return 0;
+}
