07d78c99d415a05a2cd0032f4831c8bd78e49cc1
Dennis Jakob <git@djakob.me>
2021-01-14 18:53:26 +0100

    added votieren.txt, modified statistics to use double

--- blatt07/include/statistics.hh
+++ blatt07/include/statistics.hh
@@ -6,9 +6,9 @@
 #include <algorithm>
 
 template <typename T>
-auto mean(const T& v)
+double mean(const T& v)
 {
-	typename T::value_type r{};
+	double r{};
 
 	for (auto&& e : v) {
 		r += e;
@@ -17,16 +17,16 @@ auto mean(const T& v)
 }
 
 template <typename T>
-auto median(T v)
+double median(T v)
 {
 	std::sort(v.begin(), v.end());
 	return v.size() % 2 ? v[v.size() / 2] : (v[v.size() / 2] + v[v.size() / 2 - 1]) / 2;
 }
 
 template <typename T>
-auto moment(const T& v, int k)
+double moment(const T& v, int k)
 {
-	typename T::value_type r{};
+	double r{};
 
 	for (auto&& e : v) {
 		r += std::pow(e, k);
@@ -35,9 +35,9 @@ auto moment(const T& v, int k)
 }
 
 template <typename T>
-auto standard_deviation(const T& v)
+double standard_deviation(const T& v)
 {
-	typename T::value_type m{mean(v)}, r{};
+	double m{mean(v)}, r{};
 
 	for (auto&& e : v) {
 		r += std::pow(e - m, 2);
@@ -46,7 +46,7 @@ auto standard_deviation(const T& v)
 }
 
 template <typename T>
-auto robust_median(const T& c)
+double robust_median(const T& c)
 {
 	std::vector<typename T::value_type> v{c.begin(), c.end()};
 
