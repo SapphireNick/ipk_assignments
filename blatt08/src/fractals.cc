e983f627f6b330addb537ba501514b2fef1b456d
Dennis Jakob <git@djakob.me>
2021-01-17 16:37:35 +0100

    added b8a3

--- blatt08/src/fractals.cc
+++ blatt08/src/fractals.cc
@@ -12,29 +12,33 @@ IterationResult iterate(Point z, Point c, double threshold, int maxIt)
 		z = (Point){z.x * z.x - z.y * z.y + c.x, 2 * z.x * z.y + c.y};
 		++i;
 	}
-	return (IterationResult){c, i};
+	return (IterationResult){z, i};
 }
 
-void mandelbrot(Canvas&& canvas, double threshold, int maxIt, std::string filename) 
+void mandelbrot(Canvas&& canvas, double threshold, int maxIt, std::string filename, bool smooth = false) 
 {
 	std::cout << "calculating mandelbrot" << '\n';
 	for (int i = 0; i < canvas.horPixels(); ++i) {
 		for (int j = 0; j < canvas.vertPixels(); ++j) {
 			IterationResult itres = iterate(Point(), canvas.coord(i, j), threshold, maxIt);
-			canvas(i, j) = itres.it == maxIt ? 0 : 36 * std::log(itres.it);
+			canvas(i, j) = itres.it == maxIt ? 0 : smooth ? 
+				36 * std::log(itres.it - std::log2(std::log(std::sqrt(itres.p.x * itres.p.x + itres.p.y * itres.p.y))/std::log(threshold))): 
+				36 * std::log(itres.it);
 		}
 	}
 	std::cout << "writing mandelbrot" << '\n';
 	canvas.write(filename);
 }
 
-void julia(Canvas&& canvas, double threshold, int maxIt, std::string filename) 
+void julia(Canvas&& canvas, double threshold, int maxIt, std::string filename, bool smooth = false) 
 {
 	std::cout << "calculating julia" << '\n';
 	for (int i = 0; i < canvas.horPixels(); ++i) {
 		for (int j = 0; j < canvas.vertPixels(); ++j) {
 			IterationResult itres = iterate(canvas.coord(i, j), canvas.center(), threshold, maxIt);
-			canvas(i, j) = itres.it == maxIt ? 0 : 36 * std::log(itres.it);
+			canvas(i, j) = itres.it == maxIt ? 0 : smooth ? 
+				36 * std::log(itres.it - std::log2(std::log(std::sqrt(itres.p.x * itres.p.x + itres.p.y * itres.p.y))/std::log(threshold))): 
+				36 * std::log(itres.it);
 		}
 	}
 	std::cout << "writing julia" << '\n';
@@ -45,5 +49,7 @@ int main(int argc, char** argv)
 {
 	mandelbrot(Canvas({-0.77568377, 0.13646737}, 0.002, 0.002, 8000, 8000), 3, 1000, "mandelbrot.pgm");
 	julia(Canvas({-0.8, 0.156}, 1, 1, 8000, 8000), 3, 1000, "julia.pgm");
+	mandelbrot(Canvas({-0.77568377, 0.13646737}, 0.002, 0.002, 8000, 8000), 3, 1000, "smandelbrot.pgm", true);
+	julia(Canvas({-0.8, 0.156}, 1, 1, 8000, 8000), 3, 1000, "sjulia.pgm", true);
 	return 0;
 }
