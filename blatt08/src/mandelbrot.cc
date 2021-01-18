0839cc78de9f91347810db7c690f70040435529a
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2021-01-18 13:47:28 +0100

    small changes

--- blatt08/src/mandelbrot.cc
+++ blatt08/src/mandelbrot.cc
@@ -11,7 +11,7 @@ void mandelbrot (Canvas& canvas, double threshold, int maxIt, std::string filena
 
 int main(int argc, char** argv)
 {
-    Canvas canv = Canvas(Point(-1, 0), 0.05, 0.05, 4000, 4000);
+    Canvas canv = Canvas(Point(-1, 0), 4, 3, 1000, 500);
     mandelbrot(canv, 2, 1000, "mandelbrot.pgm");
 }
 
@@ -20,19 +20,19 @@ IterationResult iterate (Point z, Point c, double threshold, int maxIt)
     IterationResult iterRes = IterationResult(z, 0);
     Point tmp;
     int x, y;
-    double distanceToOrigin = std::sqrt( std::pow(z.x(), 2) + std::pow(z.y(), 2));
+    double distanceToOrigin_sq = std::pow(z.x(), 2) + std::pow(z.y(), 2);
 
     for (int i = 0; i < maxIt; i++)
     {
         x = iterRes.getPoint().x();
         y = iterRes.getPoint().y();
-        if (distanceToOrigin > threshold) return iterRes;
+        if (distanceToOrigin_sq > threshold * threshold) return iterRes;
 
         tmp = Point(x * x - y * y + c.x(),
                     2 * x * y + c.y());
         iterRes.getPoint() = tmp;
         iterRes.getPerfIter()++;
-        distanceToOrigin = std::sqrt( x * x + y * y );
+        distanceToOrigin_sq = x * x + y * y;
     }
 
     return iterRes;
@@ -48,7 +48,7 @@ void mandelbrot (Canvas& canvas, double threshold, int maxIt, std::string filena
     {
         for(int j = 0; j < height; j++)
         {
-            res = iterate(canvas.coord(i, j), canvas.center(),
+            res = iterate(Point(), canvas.coord(i, j),
                                             threshold, maxIt);
             if (res.getPerfIter() == maxIt)
             {
